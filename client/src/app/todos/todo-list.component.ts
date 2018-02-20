ca5c9e57891cc66b773d5b55b5c526dcc74d504a
fossx229 <fossx229@morris.umn.edu>
2018-02-20 15:25:10 -0600

    Reformed to fit more in line todos rather than users.. again.. again! Fantastic!
    -Blake and Kyle

---
+++ client/src/app/todos/todo-list.component.ts
@@ -0,0 +1,137 @@
+import {Component, OnInit} from '@angular/core';
+import {TodoListService} from "./todo-list.service";
+import {Todo} from "./todo";
+import {Observable} from "rxjs";
+import {MatDialog} from '@angular/material';
+import {AddTodoComponent} from "./add-todo.component"
+
+@Component({
+    selector: 'todo-list-component',
+    templateUrl: 'todo-list.component.html',
+    styleUrls: ['./todo-list.component.css'],
+})
+
+export class TodoListComponent implements OnInit {
+    //These are public so that tests can reference them (.spec.ts)
+    public todos: Todo[];
+    public filteredTodos: Todo[];
+
+    public todoOwner : string;
+    public todoCategory : string;
+    public todoStatus : string;
+    public todoBody: string;
+
+    //For testing purposes.
+    public todoID: string;
+
+    public loadReady: boolean = false;
+
+    //Inject the UserListService into this component.
+    //That's what happens in the following constructor.
+    //panelOpenState: boolean = false;
+    //We can call upon the service for interacting
+    //with the server.
+    constructor(public todoListService: TodoListService, public dialog: MatDialog) {
+
+    }
+
+    openDialog(): void {
+        let dialogRef = this.dialog.open(AddTodoComponent, {
+            width: '500px',
+        });
+
+        dialogRef.afterClosed().subscribe(result => {
+            console.log('The dialog was closed');
+        });
+    }
+
+
+    public filterTodos(searchOwner: string, searchCategory: string, searchStatus: string, searchBody: string, searchID: string): Todo[] {
+
+        this.filteredTodos = this.todos;
+
+        //Filter by owner
+        if (searchOwner != null) {
+            searchOwner = searchOwner.toLocaleLowerCase();
+
+            this.filteredTodos = this.filteredTodos.filter(todo => {
+                return !searchOwner || todo.owner.toLowerCase().indexOf(searchOwner) !== -1;
+            });
+        }
+
+        //Filter by category
+        if (searchCategory != null) {
+            this.filteredTodos = this.filteredTodos.filter(todo => {
+                return !searchCategory || todo.category.toLowerCase().indexOf(searchCategory) !== -1;
+            });
+        }
+
+        //Filter by status
+        if (searchStatus != null) {
+            this.filteredTodos = this.filteredTodos.filter(todo => {
+                return !searchStatus || todo.status.toLowerCase().indexOf(searchStatus) !== -1;
+            });
+        }
+
+        //Filter by body
+        if (searchBody != null) {
+            this.filteredTodos = this.filteredTodos.filter(todo => {
+                return !searchBody || todo.body.toLowerCase().indexOf(searchBody) !== -1;
+            });
+        }
+
+        //Filter by id
+        if (searchID != null) {
+            this.filteredTodos = this.filteredTodos.filter(todo => {
+                return !searchID || todo._id.toLowerCase().indexOf(searchID) !== -1;
+            });
+        }
+
+
+
+        return this.filteredTodos;
+    }
+
+    /**
+     * Starts an asynchronous operation to update the users list
+     *
+     */
+    refreshTodos(): Observable<Todo[]> {
+        //Get Users returns an Observable, basically a "promise" that
+        //we will get the data from the server.
+        //
+        //Subscribe waits until the data is fully downloaded, then
+        //performs an action on it (the first lambda)
+
+        let todos : Observable<Todo[]> = this.todoListService.getTodos();
+        todos.subscribe(
+            todos => {
+                this.todos = todos;
+                this.filterTodos(this.todoOwner, this.todoCategory, this.todoStatus, this.todoBody, this.todoID);
+            },
+            err => {
+                console.log(err);
+            });
+        return todos;
+    }
+
+
+    loadService(): void {
+        this.loadReady = true;
+        this.todoListService.getTodos(this.todoCategory).subscribe(
+            todos => {
+                this.todos = todos;
+                this.filteredTodos = this.todos;
+            },
+            err => {
+                console.log(err);
+            }
+        );
+    }
+
+
+    ngOnInit(): void {
+        this.refreshTodos();
+        this.loadService();
+    }
+}
---
+++ client/src/app/todos/todo-list.service.ts
@@ -0,0 +1,79 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+import {Observable} from "rxjs";
+
+import {Todo} from './todo';
+import {environment} from "../../environments/environment";
+
+
+@Injectable()
+export class TodoListService {
+    readonly baseUrl: string = environment.API_URL + "todos";
+    private todoUrl: string = this.baseUrl;
+
+    constructor(private http: HttpClient) {
+    }
+
+    getTodos(todoCategory?: string): Observable<Todo[]> {
+        this.filterByCategory(todoCategory);
+        return this.http.get<Todo[]>(this.todoUrl);
+    }
+
+    getTodoById(id: string): Observable<Todo> {
+        return this.http.get<Todo>(this.todoUrl + "/" + id);
+    }
+
+    /*
+    //This method looks lovely and is more compact, but it does not clear previous searches appropriately.
+    //It might be worth updating it, but it is currently commented out since it is not used (to make that clear)
+    getUsersByCompany(userCompany?: string): Observable<User> {
+        this.userUrl = this.userUrl + (!(userCompany == null || userCompany == "") ? "?company=" + userCompany : "");
+        console.log("The url is: " + this.userUrl);
+        return this.http.request(this.userUrl).map(res => res.json());
+    }
+    */
+
+    filterByCategory(todoCategory?: string): void {
+        if(!(todoCategory == null || todoCategory == "")){
+            if (this.todoUrl.indexOf('category=') !== -1){
+                //there was a previous search by company that we need to clear
+                let start = this.todoUrl.indexOf('category=');
+                let end = this.todoUrl.indexOf('&', start);
+                this.todoUrl = this.todoUrl.substring(0, start-1) + this.todoUrl.substring(end+1);
+            }
+            if (this.todoUrl.indexOf('&') !== -1) {
+                //there was already some information passed in this url
+                this.todoUrl += 'category=' + todoCategory + '&';
+            }
+            else {
+                //this was the first bit of information to pass in the url
+                this.todoUrl += "?category=" + todoCategory + "&";
+            }
+        }
+        else {
+            //there was nothing in the box to put onto the URL... reset
+            if (this.todoUrl.indexOf('category=') !== -1){
+                let start = this.todoUrl.indexOf('category=');
+                let end = this.todoUrl.indexOf('&', start);
+                if (this.todoUrl.substring(start-1, start) === '?'){
+                    start = start-1
+                }
+                this.todoUrl = this.todoUrl.substring(0, start) + this.todoUrl.substring(end+1);
+            }
+        }
+    }
+
+    /*
+    This needs to be thought about further, about how we will use adding an additional ID system???
+    Currently, it is only using owner, category, status, and body, but not id.
+    */
+
+    addNewTodo(owner: string, category: string, status: string, body: string): Observable<Boolean> {
+        const contense = {owner:owner, category:category, status:status, body:body};
+        console.log(contense);
+
+        //Send post request to add a new user with the user data as the body with specified headers.
+        return this.http.post<Boolean>(this.todoUrl + "/new", body);
+    }
+}
