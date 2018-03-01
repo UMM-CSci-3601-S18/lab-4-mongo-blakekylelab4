55609e59c867befcf84565353adc5f024ec4572e
bella148 <bella148@morris.umn.edu>
2018-02-28 19:02:50 -0600

    Added additional methods getTodosByOwner and filterByOwner to address a conflict involving test code and how the rest of our software was deigned.
    -blake

--- client/src/app/todos/todo-list.service.ts
+++ client/src/app/todos/todo-list.service.ts
@@ -21,8 +21,45 @@ export class TodoListService {
         return this.http.get<Todo[]>(this.todoUrl);
     }
 
+    getTodosByOwner(todoOwner?: string): Observable<Todo[]>{
+        this.filterByOwner(todoOwner);
+        return this.http.get<Todo[]>(this.todoUrl);
+    }
+
     getTodoById(id: string): Observable<Todo> {
         return this.http.get<Todo>(this.todoUrl + '/' + id);
+    }
+
+    filterByOwner(todoOwner?: string): void {
+        if (!(todoOwner == null || todoOwner === '')) {
+
+
+            if (this.parameterPresent('owner=') ) {
+
+                // there was a previous search by category that we need to clear
+                this.removeParameter('owner=');
+            }
+            if (this.todoUrl.indexOf('?') !== -1) {
+
+                // there was already some information passed in this url
+                this.todoUrl += 'owner=' + todoOwner + '&';
+            } else {
+                // this was the first bit of information to pass in the url
+                this.todoUrl += '?owner=' + todoOwner + '&';
+            }
+
+        } else {
+
+            // there was nothing in the box to put onto the URL... reset
+            if (this.parameterPresent('owner=')) {
+                let start = this.todoUrl.indexOf('owner=');
+                const end = this.todoUrl.indexOf('&', start);
+                if (this.todoUrl.substring(start - 1, start) === '?') {
+                    start = start - 1;
+                }
+                this.todoUrl = this.todoUrl.substring(0, start) + this.todoUrl.substring(end + 1);
+            }
+        }
     }
 
 
